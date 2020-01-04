ca17509f9923a0f96f3f1579bb88f0cb2ea93667
tamotsu ichiba <ichibat@gmail.com>
2020-01-04 13:17:28 +0900

    improved

--- vagrant/ubuntu64_18/workspace/express_mongodb/index.js
+++ vagrant/ubuntu64_18/workspace/express_mongodb/index.js
@@ -2,13 +2,12 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 
+const User = require('./models/User');
+
+const options = { useNewUrlParser: true, useUnifiedTopology: true }
 port = 3000;
 
-mongoose.connect('mongodb://127.0.0.1/test_db',{
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-  }
-);
+mongoose.connect('mongodb://127.0.0.1/test_db', options);
 
 const db = mongoose.connection;
 
@@ -18,6 +17,11 @@ db.once('open', () => console.log('DB connection successful'));
 app.get('/', (req, res) => {
   res.send('Good afternoon!');
 });
+
+app.get('/user', async (req, res) => {
+  const users = await User.find({});
+  res.json(users);
+})
 
 app.listen(port, (req, res) => {
   console.log(`port listening on ${port}!`);
