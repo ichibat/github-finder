338bdd3af2a3bdaa0c6dea4249bba356c673b2c1
tamotsu ichiba <ichibat@gmail.com>
2020-05-09 10:54:03 +0900

    after avatr url

--- Dropbox/Apps/Byword/github-finder/src/App.js
+++ Dropbox/Apps/Byword/github-finder/src/App.js
@@ -1,15 +1,16 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import UserItem from './components/users/UserItem';
 import './App.css';
 
 class App extends Component {
   render() {
-    return ( < div className = "App" >
-    < h1 > Hello from React! </h1>
-
-    </div>
-  );
+    return ( <div className = 'App'>
+      <Navbar />
+      <UserItem />
+      </div>
+    );
   }
-  
 }
 
 export default App;
