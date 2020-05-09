f696850763a6cbc2d855f74a1d4649a3f3c6f409
tamotsu ichiba <ichibat@gmail.com>
2020-05-10 07:51:29 +0900

    after lesson 13

--- Dropbox/Apps/Byword/github-finder/src/components/users/UserItem.js
+++ Dropbox/Apps/Byword/github-finder/src/components/users/UserItem.js
@@ -1,12 +1,7 @@
-import React, { Component } from 'react'
-
- class UserItem extends Component {
-
-
-  render() {
-    const { login, avatar_url,html_url  } = this.props.user;
-
-
+import React from 'react';
+import PropTypes from 'prop-types';
+ 
+const UserItem = ({user: { login, avatar_url,html_url }}) => { 
     return (
       <div className="card text-center">
         <img src={avatar_url} alt="" className ="round-img" style={{ width: '60px'}}/>
@@ -15,8 +10,11 @@ import React, { Component } from 'react'
           <a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
         </div>
       </div>
-    )
-  }
+    );
+};
+
+UserItem.propTypes = {
+  user: PropTypes.object.isRequired
 }
 
 export default UserItem
