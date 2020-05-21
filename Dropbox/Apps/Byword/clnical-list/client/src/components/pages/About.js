03d7d97ddf27d8a031f546130d772ae97ef649ee
tamotsu ichiba <ichibat@gmail.com>
2020-05-22 07:57:45 +0900

    after lesson 51

---
+++ Dropbox/Apps/Byword/clnical-list/client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+export const About = () => {
+  return (
+    <div>
+      <h1>このサイトに関して</h1>
+      <p className="my-1">このサイトは臨床情報の管理目的で作成されています</p>
+      <p className="bg-dark p">
+        <strong>Version:</strong>1.0.0
+      </p>
+    </div>
+  )
+}
+
+
+
+export default About;
