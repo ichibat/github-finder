5c7e57b43b7190f7339e0659074b76d8ad07bb67
tamotsu ichiba <ichibat@gmail.com>
2020-05-22 14:43:04 +0900

    after lesson 53

--- Dropbox/Apps/Byword/clnical-list/client/src/components/layout/Navbar.js
+++ Dropbox/Apps/Byword/clnical-list/client/src/components/layout/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = ({ title, icon }) => {
   return (
     <div className="navbar bg-primary">
       <h1>
-      <Link to='/'><i className={icon} />{title}</Link>
+      <Link to='/'><i className={icon} />{' '}{title}</Link>
       </h1>
       <ul>
         <li>
